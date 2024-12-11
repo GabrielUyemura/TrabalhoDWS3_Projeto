6163ea547c416f10a268c8804caf46ecadba3ce1
Burnunssi <j.burnunssi@aluno.ifsp.edu.br>
2024-12-11 11:11:12 -0300

    Adicionando Rotas do backend

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const routerApp = express.Router();
 
-const appDisciplina = require("../apps/disciplina/controller/ctlDisciplina");
 const appLogin = require("../apps/login/controller/ctlLogin");
+const appCurso = require("../apps/curso/controller/ctlCurso");
+const appDisciplina = require("../apps/disciplina/controller/ctlDisciplina");
+const appMatricula = require("../apps/matricula/controller/ctlMatricula");
 
 // middleware that is specific to this router
 routerApp.use((req, res, next) => {
@@ -13,12 +15,26 @@ routerApp.get("/", (req, res) => {
   res.send("Olá mundo!");
 });
 
+//Rotas de Curso
+routerApp.get("/getAllCurso", appLogin.autenticaJWT, appCurso.getAllCurso);
+routerApp.post("/getCursoByID", appLogin.autenticaJWT, appCurso.getCursoByID);
+routerApp.post("/insertCurso", appLogin.autenticaJWT, appCurso.insertCurso);
+routerApp.post("/updateCurso", appLogin.autenticaJWT, appCurso.updateCurso);
+routerApp.post("/deleteCurso", appLogin.autenticaJWT, appCurso.deleteCurso);
+
 //Rotas de Disciplina
 routerApp.get("/getAllDisciplina", appLogin.autenticaJWT, appDisciplina.getAllDisciplina);
 routerApp.post("/getDisciplinaByID", appLogin.autenticaJWT, appDisciplina.getDisciplinaByID);
 routerApp.post("/insertDisciplina", appLogin.autenticaJWT, appDisciplina.insertDisciplina);
 routerApp.post("/updateDisciplina", appLogin.autenticaJWT, appDisciplina.updateDisciplina);
 routerApp.post("/deleteDisciplina", appLogin.autenticaJWT, appDisciplina.deleteDisciplina);
+
+//Rotas de Matricula
+routerApp.get("/getAllMatricula", appLogin.autenticaJWT, appMatricula.getAllMatricula);
+routerApp.post("/getMatriculaByID", appLogin.autenticaJWT, appMatricula.getMatriculaByID);
+routerApp.post("/insertMatricula", appLogin.autenticaJWT, appMatricula.insertMatricula);
+routerApp.post("/updateMatricula", appLogin.autenticaJWT, appMatricula.updateMatricula);
+routerApp.post("/deleteMatricula", appLogin.autenticaJWT, appMatricula.deleteMatricula);
 
 // Rota Login
 routerApp.post("/login", appLogin.login);
