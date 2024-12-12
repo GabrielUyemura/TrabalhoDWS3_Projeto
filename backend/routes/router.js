a137554fe9ce32f2cba8ff09ffe6d3d23eacf685
Gabriel <japauyemura123@gmail.com>
2024-12-11 21:55:40 -0300

    Atualização no arquivo de rotas

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -4,8 +4,8 @@ const routerApp = express.Router();
 const appLogin = require("../apps/login/controller/ctlLogin");
 const appCurso = require("../apps/curso/controller/ctlCurso");
 const appAluno = require("../apps/aluno/controller/ctlAluno");
-// const appDisciplina = require("../apps/disciplina/controller/ctlDisciplina");
-// const appMatricula = require("../apps/matricula/controller/ctlMatricula");
+const appDisciplina = require("../apps/disciplina/controller/ctlDisciplina");
+const appMatricula = require("../apps/matricula/controller/ctlMatricula");
 
 // middleware that is specific to this router
 routerApp.use((req, res, next) => {
@@ -30,19 +30,19 @@ routerApp.post("/insertCurso", appLogin.autenticaJWT, appCurso.insertCurso);
 routerApp.put("/updateCurso", appLogin.autenticaJWT, appCurso.updateCurso);
 routerApp.delete("/deleteCurso", appLogin.autenticaJWT, appCurso.deleteCurso);
 
-// //Rotas de Disciplina
-// routerApp.get("/getAllDisciplina", appLogin.autenticaJWT, appDisciplina.getAllDisciplina);
-// routerApp.post("/getDisciplinaByID", appLogin.autenticaJWT, appDisciplina.getDisciplinaByID);
-// routerApp.post("/insertDisciplina", appLogin.autenticaJWT, appDisciplina.insertDisciplina);
-// routerApp.put("/updateDisciplina", appLogin.autenticaJWT, appDisciplina.updateDisciplina);
-// routerApp.delete("/deleteDisciplina", appLogin.autenticaJWT, appDisciplina.deleteDisciplina);
+//Rotas de Disciplina
+routerApp.get("/getAllDisciplina", appLogin.autenticaJWT, appDisciplina.getAllDisciplina);
+routerApp.post("/getDisciplinaByID", appLogin.autenticaJWT, appDisciplina.getDisciplinaByID);
+routerApp.post("/insertDisciplina", appLogin.autenticaJWT, appDisciplina.insertDisciplina);
+routerApp.put("/updateDisciplina", appLogin.autenticaJWT, appDisciplina.updateDisciplina);
+routerApp.delete("/deleteDisciplina", appLogin.autenticaJWT, appDisciplina.deleteDisciplina);
 
-// //Rotas de Matricula
-// routerApp.get("/getAllMatricula", appLogin.autenticaJWT, appMatricula.getAllMatricula);
-// routerApp.post("/getMatriculaByID", appLogin.autenticaJWT, appMatricula.getMatriculaByID);
-// routerApp.post("/insertMatricula", appLogin.autenticaJWT, appMatricula.insertMatricula);
-// routerApp.put("/updateMatricula", appLogin.autenticaJWT, appMatricula.updateMatricula);
-// routerApp.delete("/deleteMatricula", appLogin.autenticaJWT, appMatricula.deleteMatricula);
+//Rotas de Matricula
+routerApp.get("/getAllMatricula", appLogin.autenticaJWT, appMatricula.getAllMatricula);
+routerApp.post("/getMatriculaByID", appLogin.autenticaJWT, appMatricula.getMatriculaByID);
+routerApp.post("/insertMatricula", appLogin.autenticaJWT, appMatricula.insertMatricula);
+routerApp.put("/updateMatricula", appLogin.autenticaJWT, appMatricula.updateMatricula);
+routerApp.delete("/deleteMatricula", appLogin.autenticaJWT, appMatricula.deleteMatricula);
 
 // Rota Login
 routerApp.post("/login", appLogin.login);
